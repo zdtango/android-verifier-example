7fdaee53ed2247ce908b70ef0b2fafa77831fe60
Zach Tang <zachtangoj@gmail.com>
2025-05-31 20:29:48 -0700

    android plugin verifier test

--- src/main/kotlin/org/jetbrains/plugins/template/MyBundle.kt
+++ src/main/kotlin/org/jetbrains/plugins/template/MyBundle.kt
@@ -3,11 +3,24 @@ package org.jetbrains.plugins.template
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
 import org.jetbrains.annotations.PropertyKey
+import com.android.io.FileWrapper
+import com.intellij.openapi.vfs.LocalFileSystem
+import com.intellij.psi.PsiManager
+import java.io.File
+import org.jetbrains.android.AndroidAnnotatorUtil
 
 @NonNls
 private const val BUNDLE = "messages.MyBundle"
 
 object MyBundle : DynamicBundle(BUNDLE) {
+
+    val ioFile = File("TEst")
+    val file = FileWrapper(ioFile)
+
+    val project = com.intellij.openapi.project.ProjectManager.getInstance().openProjects.firstOrNull()
+    val virtualFile = LocalFileSystem.getInstance().findFileByIoFile(ioFile)
+    val psiFile = virtualFile?.let { project?.let { PsiManager.getInstance(it).findFile(virtualFile) } }
+    val annotator = AndroidAnnotatorUtil.getFileType(psiFile!!)
 
     @JvmStatic
     fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any) =
